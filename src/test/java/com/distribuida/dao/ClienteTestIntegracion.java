e49ddb805833c02939b8a360c5b824457b9fcbcc
1719432278 <cvega@tecnologicoismac.edu.ec>
2025-11-28 03:20:47 +0100

    FacturaTestIntegracion 27/11/2025

--- src/test/java/com/distribuida/dao/ClienteTestIntegracion.java
+++ src/test/java/com/distribuida/dao/ClienteTestIntegracion.java
@@ -3,6 +3,7 @@ package com.distribuida.dao;
 import com.distribuida.model.Cliente;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,6 +11,9 @@ import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @DataJpaTest
@@ -24,24 +28,37 @@ import java.util.Optional;
         @Test
         public void testClienteFindAll(){
             List<Cliente> clientes =clienteRepository.findAll();
+            assertNotNull (clientes);
+            asserTrue( clientes.size()>0);
             for(Cliente item: clientes){
                 System.out.println(item.toString());
             }
         }
-@Test
+
+    private void asserTrue(boolean b) {
+    }
+
+    @Test
         public void testClienteFindOne(){
     Optional<Cliente> cliente = clienteRepository.findById(1);
+    assertNotNull(cliente.isPresent());
+    assertEquals("Puro",cliente.orElse(null).getNombre());
+    assertEquals("Hueso",cliente.orElse(null).getApellido());
     System.out.println((cliente));
 }
 @Test
     public  void testClienteSave(){
             Cliente cliente =new Cliente(0,"1719432278","Juan","Taipe","Tumbaco","0990464220","jtaipe@gmail.com");
-clienteRepository.save(cliente);
+            Cliente clienteGuardado =clienteRepository.save(cliente);
+            assertNotNull(clienteGuardado);
+            assertEquals("1719432278",clienteGuardado.getCedula());
+            assertEquals("Juan",clienteGuardado.getNombre());
         }
 
 @Test
 public  void testClienteActualizar(){
             Optional<Cliente>cliente=clienteRepository.findById(39);
+
             cliente.orElse(null).setCedula("1719412245");
             cliente.orElse(null).setNombre("Jose");
             cliente.orElse(null).setApellido("Diaz");
