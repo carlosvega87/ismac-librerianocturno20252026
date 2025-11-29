6a9dec5c192f47f087e8c010d8b0e7c0f07c71b7
1719432278 <cvega@tecnologicoismac.edu.ec>
2025-11-29 03:02:33 +0100

    TestIntegracion 28/11/2025

--- src/test/java/com/distribuida/dao/ClienteTestIntegracion.java
+++ src/test/java/com/distribuida/dao/ClienteTestIntegracion.java
@@ -12,8 +12,7 @@ import org.springframework.test.annotation.Rollback;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @DataJpaTest
@@ -22,55 +21,71 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
     @Rollback(value= false)
     public class ClienteTestIntegracion {
 
-        @Autowired
-        private ClienteRepository clienteRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-        @Test
-        public void testClienteFindAll(){
-            List<Cliente> clientes =clienteRepository.findAll();
-            assertNotNull (clientes);
-            asserTrue( clientes.size()>0);
-            for(Cliente item: clientes){
-                System.out.println(item.toString());
-            }
+    @Test
+    public void testClienteFindAll() {
+        List<Cliente> clientes = clienteRepository.findAll();
+        assertNotNull(clientes);
+        asserTrue(clientes.size() > 0);
+        for (Cliente item : clientes) {
+            System.out.println(item.toString());
         }
+    }
 
     private void asserTrue(boolean b) {
     }
 
     @Test
-        public void testClienteFindOne(){
-    Optional<Cliente> cliente = clienteRepository.findById(1);
-    assertNotNull(cliente.isPresent());
-    assertEquals("Puro",cliente.orElse(null).getNombre());
-    assertEquals("Hueso",cliente.orElse(null).getApellido());
-    System.out.println((cliente));
-}
-@Test
-    public  void testClienteSave(){
-            Cliente cliente =new Cliente(0,"1719432278","Juan","Taipe","Tumbaco","0990464220","jtaipe@gmail.com");
-            Cliente clienteGuardado =clienteRepository.save(cliente);
-            assertNotNull(clienteGuardado);
-            assertEquals("1719432278",clienteGuardado.getCedula());
-            assertEquals("Juan",clienteGuardado.getNombre());
-        }
-
-@Test
-public  void testClienteActualizar(){
-            Optional<Cliente>cliente=clienteRepository.findById(39);
-
-            cliente.orElse(null).setCedula("1719412245");
-            cliente.orElse(null).setNombre("Jose");
-            cliente.orElse(null).setApellido("Diaz");
-            cliente.orElse(null).setDireccion("Pifo");
-            cliente.orElse(null).setTelefono("2056431");
-            cliente.orElse(null).setCorreo("jdiaz@gmail.com");
-            clienteRepository.save(cliente.orElse(null));
-
-}
-@Test
-    public void testClienteBorrar(){
-            clienteRepository.deleteById(39);
-}
+    public void testClienteFindOne() {
+        Optional<Cliente> cliente = clienteRepository.findById(1);
+        assertNotNull(cliente.isPresent());
+        assertEquals("Puro", cliente.orElse(null).getNombre());
+        assertEquals("Hueso", cliente.orElse(null).getApellido());
+        System.out.println((cliente));
     }
+
+    @Test
+    public void testClienteSave() {
+        Cliente cliente = new Cliente(0, "1719432278", "Juan", "Taipe", "Tumbaco", "0990464220", "jtaipe@gmail.com");
+        Cliente clienteGuardado = clienteRepository.save(cliente);
+        assertNotNull(clienteGuardado);
+        assertEquals("1719432278", clienteGuardado.getCedula());
+        assertEquals("Juan", clienteGuardado.getNombre());
+    }
+
+    @Test
+    public void testClienteActualizar() {
+        Optional<Cliente> cliente2 = clienteRepository.findById(39);
+        asserTrue(cliente2.isPresent());
+        cliente2.orElse(null).setCedula("1719412245");
+        cliente2.orElse(null).setNombre("Jose");
+        cliente2.orElse(null).setApellido("Diaz");
+        cliente2.orElse(null).setDireccion("Pifo");
+        cliente2.orElse(null).setTelefono("2056431");
+        cliente2.orElse(null).setCorreo("jdiaz@gmail.com");
+        Cliente clienteActualizado = clienteRepository.save(cliente2.orElse(null));
+
+        assertNotNull(clienteActualizado);
+        assertEquals("Diaz", clienteActualizado.getApellido());
+        assertEquals("Pifo", clienteActualizado.getDireccion());
+    }
+
+    @Test
+    public void testClienteBorrar() {
+        clienteRepository.deleteById(39);
+        if(clienteRepository.existsById(39)){
+       clienteRepository.deleteById(39);
+    }
+    assertFalse(clienteRepository.existsById(39));
+
+
+    }
+    }
+
+
+
+
+
 
--- src/test/java/com/distribuida/dao/FacturaTestIntegracion.java
+++ src/test/java/com/distribuida/dao/FacturaTestIntegracion.java
@@ -13,6 +13,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
@@ -28,34 +30,53 @@ public class FacturaTestIntegracion {
     @Test
     public void testFacturaFindAll(){
         List<Factura> facturas = facturaRepository.findAll();
+        assertNotNull(facturas);
+        assertTrue(facturas.size()>0);
+
         facturas.forEach(System.out::println);
     }
     @Test
     public void testFacturaFindOne(){
         Optional<Factura> factura = facturaRepository.findById(1);
+assertTrue(factura.isPresent());
+assertEquals("FAC-0001",factura.orElse(null).getNumFactura());
+assertEquals(150.96,factura.orElse(null).getTotal());
 
-        System.out.println(factura);
+System.out.println(factura);
     }
 
     @Test
     public void testFacturaSave(){
         Optional<Cliente> cliente = clienteRepository.findById(1);
+        assertTrue(cliente.isPresent());
+
+
+
         Factura factura=new Factura();
         factura.setIdFactura(0);
         factura.setNumFactura("FAC-00066");
         factura.setFecha(new Date());
-        factura.setTotalNeto(10.00);
+        factura.setTotalNeto(100.00);
         factura.setIva(15.00);
         factura.setTotal(115.00);
         factura.setCliente(cliente.orElse(null));
 
-        facturaRepository.save(factura);
+        Factura facturaGuardada= facturaRepository.save(factura);
+        assertNotNull(facturaGuardada);
+        assertEquals("FAC-00066",facturaGuardada.getNumFactura());
+        assertEquals(100.00, facturaGuardada.getTotalNeto());
 
     }
     @Test
     public void testFacturaUpdate(){
         Optional<Cliente> cliente = clienteRepository.findById(2);
-        Optional<Factura> factura=facturaRepository.findById(86);
+
+        assertTrue(cliente.isPresent());
+
+        Optional<Factura> factura=facturaRepository.findById(89);
+
+        assertTrue((factura.isPresent()));
+
 
         factura.orElse(null).setNumFactura("FAC-00077");
         factura.orElse(null).setFecha(new Date());
@@ -64,11 +85,17 @@ public class FacturaTestIntegracion {
         factura.orElse(null).setTotal(260.00);
         factura.orElse(null).setCliente(cliente.orElse(null));
 
-        facturaRepository.save(factura.orElse(null));
+        Factura facturaActualizada =facturaRepository.save(factura.orElse(null));
+        assertNotNull(facturaActualizada);
+        assertEquals("FAC-00077 ",facturaActualizada.getNumFactura());
+        assertEquals(260.00,facturaActualizada.getTotal());
     }
     @Test
     public void testFacturaDelete(){
-        facturaRepository.deleteById(86);
+        if (facturaRepository.existsById(89)){
+            facturaRepository.deleteById(89);
+        }
+        assertFalse(facturaRepository.existsById(89));
     }
 
 }
