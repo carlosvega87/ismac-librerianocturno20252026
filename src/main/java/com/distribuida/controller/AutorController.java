01cf2f50e1d93df84da6d92be8858ef2fc4cffdc
1719432278 <cvega@tecnologicoismac.edu.ec>
2026-01-08 03:19:41 +0100

    Contoller act.

--- src/main/java/com/distribuida/controller/AutorController.java
+++ src/main/java/com/distribuida/controller/AutorController.java
@@ -4,10 +4,7 @@ import com.distribuida.model.Autor;
 import com.distribuida.service.AutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +28,14 @@ public class AutorController {
             return  ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(autor.orElse(null));
-
     }
+
+    @PostMapping
+    public ResponseEntity<Autor> save(@RequestBody Autor autor){
+        Autor autorNuevo = autorService.save(autor);
+        return  ResponseEntity.ok(autorNuevo);
+    }
+
+
+
 }
